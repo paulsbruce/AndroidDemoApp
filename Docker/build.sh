893e8c58730eafb706eb9d6eebbb9a324be69765
Paul Bruce <me@paulsbruce.io>
2017-03-03 20:54:52 -0500

    Triggering build

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,1 +1,4 @@
 docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master --volumes-from=jenkins-data -d jenkins2
+
+## or if local directory mounted for jenkins config
+# docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master -v ~/docker/jenkins2/var/jenkins_home:/var/jenkins_home -d jenkins2
