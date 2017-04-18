8d0ea8cd6eab56951af83ceb246bec40957e67f6
Paul Bruce <me@paulsbruce.io>
2017-04-18 16:18:13 -0400

    Triggering

---
+++ Jenkins/callPerfectoFromPipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+GIT_NAME=$(git --no-pager show -s --format='%an' $GIT_COMMIT)
+GIT_EMAIL=$(git --no-pager show -s --format='%ae' $GIT_COMMIT)
+echo "CONTRIBUTOR: $GIT_NAME $GIT_EMAIL ($GIT_COMMIT)"
+cd ${WORKSPACE}
+curl -s https://raw.githubusercontent.com/paulsbruce/AndroidDemoApp/master/Jenkins/runSmallTests.sh > ./Perfecto_API.sh
+chmod u+x ./Perfecto_API.sh
+bash Perfecto_API.sh "${WORKSPACE}" "app-debug" "com.example.perfecto.tipcalculator.test" "${PERFECTO_CLOUD}" "${PERFECTO_USERNAME}" "${PERFECTO_PASSWORD}" "/" "2" "$GIT_EMAIL" "${PERFECTO_REPORTING_KEY}"
+exit $?
